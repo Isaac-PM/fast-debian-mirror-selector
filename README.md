e0121ee0514da141c71f40739237cd7d219fe0de
Isaac-PM <isaacpalma21@gmail.com>
2023-01-14 19:48:08 -0600

    merge

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # fast-debian-mirror-selector
 Finds the best Debian mirror for a location.
+
+The script finds the best Debian mirror for a location by measuring response time of a list of mirrors stored in arrays. The naming convention for arrays is 'country/territory_name_mirrors' for example Denmark_mirrors, France_mirrors. It iterates through mirrors, pinging each once, and records response time. It then selects the fastest mirror, prints its URL and outputs it.
+
+## User's guide
+
+### Downloading the script
+
+The script can be downloaded by cloning this repository: `git clone https://github.com/Isaac-PM/fast-debian-mirror-selector`, or by using a third-party service like [DownGit](https://minhaskamal.github.io/DownGit/#/home).
+
+### Running the script
+
+Depending in the script was dowloades, it may need to be extracted first. 
+
+After that is done, the script needs to be made executable by running `chmod +x fast-debian-mirror-selector.sh` in the terminal.
+
+Finally, it can be run by executing `./fast-debian-mirror-selector.sh` in the terminal.
+
+### Using the script
+
+The script will print a numbered list of countries/territories, like this one:
+
+```
+1. Afghanistan
+2. Albania
+3. Algeria
+4. ...
+```
+<center><img   src="https://i.imgur.com/yFeyfFz.png"   width=""   height=""   /></center>
+
+For selecting the countries that would like to be teste, the user needs to type the numbers of the countries, separated by commas, and press enter. For example, if the user wants to test the mirrors in the Albania and Algeria, they would type `1, 2` and press enter.
+
+<center><img   src="https://i.imgur.com/InbKgRm.png"   width=""   height=""   /></center>
+
+Then the script will `ping` each mirror in the selected countries, and print the results. After that, it will print the URL of the fastests mirrors.
+
+<center><img   src="https://i.imgur.com/ClgL9IB.png"   width=""   height=""   /></center>
---
+++ best-debian-mirror.sh
@@ -0,0 +1,309 @@
+#!/bin/bash
+
+# Mirror list
+Argentina_mirrors=("debian.unnoba.edu.ar" "mirror.sitsa.com.ar")
+
+Armenia_mirrors=("ftp.am.debian.org" "mirrors.asnet.am ")
+
+Australia_mirrors=("ftp.au.debian.org" "debian.mirror.digitalpacific.com.au" "debian.mirror.serversaustralia.com.au" "mirror.aarnet.edu.au" "mirror.amaze.com.au" "mirror.linux.org.au" "mirror.overthewire.com.au" "mirror.realcompute.io")
+
+Austria_mirrors=("ftp.at.debian.org" "debian.anexia.at" "debian.lagis.at" "debian.mur.at" "debian.sil.at" "ftp.tu-graz.ac.at" "mirror.alwyzon.net")
+
+Belarus_mirrors=("ftp.by.debian.org" "ftp.byfly.by" "mirror.datacenter.by")
+
+Belgium_mirrors=("ftp.be.debian.org" "ftp.belnet.be" "mirror.as35701.net")
+
+Brazil_mirrors=("ftp.br.debian.org" "alcateia.ufscar.br" "debian.c3sl.ufpr.br" "debian.itsbrasil.net" "debian.pop-sc.rnp.br" "mirror.uepg.br" "mirror.unesp.br")
+
+Bulgaria_mirrors=("ftp.bg.debian.org" "debian.ipacct.com" "debian.mnet.bg" "debian.telecoms.bg" "ftp.uni-sofia.bg" "mirror.telepoint.bg")
+
+Cambodia_mirrors=("mirror.sabay.com.kh")
+
+Canada_mirrors=("ftp.ca.debian.org" "debian.mirror.iweb.ca" "debian.mirror.rafal.ca" "mirror.csclub.uwaterloo.ca" "mirror.estone.ca" "mirror.it.ubc.ca")
+
+Chile_mirrors=("ftp.cl.debian.org" "debian.redlibre.cl" "mirror.insacom.cl" "mirror.ufro.cl")
+
+Costa_Rica_mirrors=("debianmirror.una.ac.cr" "mirrors.ucr.ac.cr")
+
+Croatia_mirrors=("ftp.hr.debian.org" "debian.carnet.hr")
+
+Czech_Republic_mirrors=("ftp.cz.debian.org" "debian.mirror.web4u.cz" "debian.superhosting.cz" "ftp.cvut.cz" "ftp.debian.cz" "ftp.zcu.cz" "merlin.fit.vutbr.cz" "mirror.dkm.cz" "mirror-prg.webglobe.com" "mirrors.nic.cz")
+
+Denmark_mirrors=("ftp.dk.debian.org" "mirror.asergo.com" "mirror.one.com" "mirrors.dotsrc.org" "mirrors.rackhosting.com")
+
+Estonia_mirrors=("mirrors.xtom.ee")
+
+Finland_mirrors=("ftp.fi.debian.org" "www.nic.funet.fi")
+
+France_mirrors=("ftp.fr.debian.org" "apt.tetaneutral.net" "debian.apt-mirror.de" "debian.obspm.fr" "debian.polytech-lille.fr" "debian.proxad.net" "deb-mir1.naitways.net" "ftp.ec-m.fr" "ftp.lip6.fr" "ftp.rezopole.net" "ftp.univ-pau.fr" "ftp.u-picardie.fr" "ftp.u-strasbg.fr" "miroir.univ-lorraine.fr" "mirror.johnnybegood.fr" "mirror.plusserver.com" "mirrors.ircam.fr")
+
+Georgia_mirrors=("debian.grena.ge")
+
+Germany_mirrors=("ftp.de.debian.org" "artfiles.org" "debian.charite.de" "debian.inf.tu-dresden.de" "debian.intergenia.de" "debian.mirror.iphh.net" "debian.mirror.lrz.de" "debian.netcologne.de" "debian.tu-bs.de" "de.mirrors.clouvider.net" "ftp.fau.de" "ftp.gwdg.de" "ftp.plusline.net" "ftp-stud.hs-esslingen.de" "ftp.tu-chemnitz.de" "ftp.tu-clausthal.de" "ftp.uni-bayreuth.de" "ftp.uni-hannover.de" "ftp.uni-kl.de" "ftp.uni-mainz.de" "ftp.wrz.de" "mirror.23m.com" "mirror.de.leaseweb.net" "mirror.dogado.de" "mirror.eu.oneandone.net" "mirror.garr.it" "mirror.informatik.rwth-aachen.de" "mirror.ipb.de" "mirror.netzwerge.de" "mirrors.xtom.de" "mirror.united-gameserver.de" "mirror.wtnet.de" "packages.hs-regensburg.de" "pubmirror.plutex.de")
+
+Greece_mirrors=("debian.otenet.gr")
+
+Hong_Kong_mirrors=("ftp.hk.debian.org" "mirror.xtom.com.hk")
+
+Hungary_mirrors=("ftp.hu.debian.org" "ftp.bme.hu" "ftp.fsn.hu" "repo.jztkft.hu")
+
+Iceland_mirrors=("ftp.is.debian.org")
+
+India_mirrors=("debian.mirror.net.in")
+
+Indonesia_mirrors=("kebo.pens.ac.id" "mirror.poliwangi.ac.id")
+
+Iran_mirrors=("debian.asis.ai" "mirrors.pardisco.co")
+
+Israel_mirrors=("debian.interhost.co.il" "mirror.isoc.org.il")
+
+Italy_mirrors=("ftp.it.debian.org" "debian.connesi.it" "debian.dynamica.it" "debian.mirror.garr.it" "ftp.linux.it" "giano.com.dist.unige.it" "mirror.units.it")
+
+Japan_mirrors=("ftp.jp.debian.org" "debian-mirror.sakura.ne.jp" "dennou-k.gfd-dennou.org" "dennou-q.gfd-dennou.org" "ftp.jaist.ac.jp" "ftp.kddilabs.jp" "ftp.riken.jp" "hanzubon.jp" "mirrors.xtom.jp")
+
+Kazakhstan_mirrors=("mirror.hoster.kz" "mirror.ps.kz")
+
+Kenya_mirrors=("debian.mirror.ac.ke" "debian.mirror.liquidtelecom.com")
+
+Latvia_mirrors=("debian.koyanet.lv" "mirror.cloudhosting.lv")
+
+Lithuania_mirrors=("ftp.lt.debian.org" "debian.balt.net" "debian.mirror.vu.lt" "mirror.litnet.lt")
+
+Luxembourg_mirrors=("debian.mirror.root.lu")
+
+Macedonia_mirrors=("mirror.onevip.mk")
+
+Moldova_mirrors=("ftp.md.debian.org" "mirror.as43289.net" "mirrors.mivocloud.com")
+
+Netherlands_mirrors=("ftp.nl.debian.org" "debian.mirror.cambrium.nl" "debian.snt.utwente.nl" "mirror.duocast.net" "mirror.i3d.net" "mirror.nforce.com" "mirror.nl.datapacket.com" "mirror.nl.leaseweb.net" "mirror.seedvps.com" "mirror.serverius.net" "mirrors.xtom.nl" "nl.mirrors.clouvider.net")
+
+New_Caledonia_mirrors=("mirror.lagoon.nc")
+
+New_Zealand_mirrors=("ftp.nz.debian.org" "mirror.fsmg.org.nz")
+
+Norway_mirrors=("ftp.no.debian.org" "ftp.uio.no")
+
+Peoples_Republic_of_China_mirrors=("ftp.cn.debian.org" "mirrors.163.com" "mirrors.bfsu.edu.cn" "mirrors.hit.edu.cn" "mirrors.huaweicloud.com" "mirrors.tuna.tsinghua.edu.cn" "mirrors.ustc.edu.cn")
+
+Poland_mirrors=("ftp.pl.debian.org" "debian.inhost.pro" "ftp.agh.edu.pl" "ftp.icm.edu.pl" "ftp.psnc.pl" "ftp.task.gda.pl")
+
+Portugal_mirrors=("ftp.pt.debian.org" "debian.uevora.pt" "ftp.eq.uc.pt" "ftp.rnl.tecnico.ulisboa.pt" "mirrors.ptisp.pt" "mirrors.up.pt")
+
+Republic_of_China_mirrors=("ftp.tw.debian.org" "debian.csie.ncku.edu.tw" "debian.csie.ntu.edu.tw" "opensource.nchc.org.tw")
+
+Reunion_mirrors=("debian.mithril.re" "depot-debian.univ-reunion.fr")
+
+Romania_mirrors=("mirror.flokinet.net/debian/" "mirrors.hostico.ro/debian/" "mirrors.nav.ro/debian/" "mirrors.nxthost.com/debian/")
+
+Russia_mirrors=("ftp.ru.debian.org/debian/" "ftp.psn.ru/debian/" "mirror.corbina.net/debian/" "mirror.docker.ru/debian/" "mirror.mephi.ru/debian/" "mirrors.powernet.com.ru/debian/" "mirror.surf/debian/" "mirror.truenetwork.ru/debian/")
+
+Romania_mirrors=("mirror.flokinet.net" "mirrors.hostico.ro" "mirrors.nav.ro" "mirrors.nxthost.com")
+
+Russia_mirrors=("ftp.ru.debian.org" "ftp.psn.ru" "mirror.corbina.net" "mirror.docker.ru" "mirror.mephi.ru" "mirrors.powernet.com.ru" "mirror.surf" "mirror.truenetwork.ru")
+
+Serbia_mirrors=("mirror.pmf.kg.ac.rs")
+
+Singapore_mirrors=("mirror.coganng.com" "mirror.djvg.sg" "mirror.sg.gs" "mirror.soonkeat.sg")
+
+Slovakia_mirrors=("ftp.sk.debian.org" "ftp.antik.sk" "ftp.debian.sk")
+
+Slovenia_mirrors=("ftp.si.debian.org")
+
+South_Africa_mirrors=("debian.saix.net" "ftp.is.co.za" "mirror.hostafrica.co.za")
+
+South_Korea_mirrors=("ftp.kr.debian.org" "ftp.kaist.ac.kr" "ftp.lanet.kr" "mirror.anigil.com" "mirror.misakamikoto.network")
+
+Spain_mirrors=("ftp.es.debian.org" "debian.grn.cat" "debian.redimadrid.es" "debian.redparra.com" "ftp.caliu.cat" "ftp.cica.es" "ftp.udc.es" "repo.ifca.es" "softlibre.unizar.es" "ulises.hostalia.com")
+
+Sweden_mirrors=("ftp.se.debian.org" "debian.lth.se" "debian.mirror.su.se" "ftp.acc.umu.se" "ftpmirror1.infania.net" "mirror.linux.pizza" "mirrors.glesys.net" "mirror.zetup.net")
+
+Switzerland_mirrors=("ftp.ch.debian.org" "debian.ethz.ch" "mirror1.infomaniak.com" "mirror2.infomaniak.com" "mirror.init7.net" "mirror.iway.ch" "mirror.sinavps.ch")
+
+Thailand_mirrors=("ftp.debianclub.org")
+
+Turkey_mirrors=("ftp.debianclub.org")
+
+Ukraine_mirrors=("debian.astra.in.ua", "debian.netforce.hosting", "mirror.mirohost.net")
+
+United_Kingdom_mirrors=("ftp.uk.debian.org" "debian.mirrors.uk2.net" "debian.mirror.uk.sargasso.net" "free.hands.com" "ftp.ticklers.org" "mirror.bytemark.co.uk" "mirror.lchost.net" "mirror.mythic-beasts.com" "mirror.ox.ac.uk" "mirror.positive-internet.com" "mirrors.coreix.net" "mirrorservice.org" "mirror.sov.uk.goscomb.net" "mirror.vorboss.net" "ukdebian.mirror.anlx.net")
+
+United_States_mirrors=("ftp.us.debian.org" "atl.mirrors.clouvider.net" "debian-archive.trafficmanager.net" "debian.cc.lehigh.edu" "debian.csail.mit.edu" "debian.cs.binghamton.edu" "debian.mirror.constant.com" "debian.osuosl.org" "debian.uchicago.edu" "la.mirrors.clouvider.net" "mirror.dal.nexril.net" "mirror.pit.teraswitch.com" "mirrors.accretive-networks.net" "mirrors.bloomu.edu" "mirrors.gigenet.com" "mirrors.lug.mtu.edu" "mirrors.namecheap.com" "mirrors.ocf.berkeley.edu" "mirror.steadfast.net" "mirrors.vcea.wsu.edu" "mirrors.wikimedia.org" "mirror.us.leaseweb.net" "mirror.us.oneandone.net" "nyc.mirrors.clouvider.net" "plug-mirror.rcac.purdue.edu" "repo.ialab.dsu.edu" "us.mirror.nsec.pt")
+
+Vietnam_mirrors=("mirror.bizflycloud.vn")
+
+# Miscellaneous functions
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+center_msg() {
+  echo $1| sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
+}
+
+break_line() {
+	msg " "
+}
+
+# Foreword
+setup_colors
+msg "${CYAN}"
+center_msg "---"
+center_msg "Fast Debian Mirror Selector"
+center_msg "---" 
+break_line
+
+msg "The script finds the best Debian mirror for a location by measuring response time of a list of mirrors stored in arrays. The naming convention for arrays is 'country/territory_name_mirrors' for example Denmark_mirrors, France_mirrors. It iterates through mirrors, pinging each once, and records response time. It then selects the fastest mirror, prints its URL and outputs it." 
+break_line
+
+
+# Define a list of countries
+countries=("Argentina" "Armenia" "Australia" "Austria" "Belarus" "Belgium" "Brazil" "Bulgaria" "Cambodia" "Canada" "Chile" "Costa_Rica" "Croatia" "Czech_Republic" "Denmark" "Estonia" "Finland" "France" "Georgia" "Germany" "Greece" "Hong_Kong" "Hungary" "Iceland" "India" "Indonesia" "Iran" "Israel" "Italy" "Japan" "Kazakhstan" "New_Caledonia" "Kenya" "Latvia" "Lithuania" "Luxembourg" "Macedonia" "Moldova" "Netherlands" "New_Zealand" "Norway" "Peoples_Republic_of_China" "Poland" "Portugal" "Republic_of_China" "Reunion" "Romania" "Russia" "Serbia" "Singapore" "Slovakia" "Slovenia" "South_Africa" "South_Korea" "Spain" "Sweden" "Switzerland" "Thailand" "Turkey" "Ukraine" "United_Kingdom" "United_States" "Vietnam")
+
+# Display the main menu
+center_msg "Main menu" 
+break_line
+
+msg "Select one or more countries by entering the corresponding number(s) separated by commas (v.g. 1, 2, 3, ...):"
+
+# Display the list of countries with numbers
+for i in "${!countries[@]}"
+do
+  printf "%d. %s\n" $((i+1)) "${countries[i]}"
+done
+
+# Read user input
+read -p "Enter your selection: " input
+break_line
+
+# Split the input into an array
+IFS=',' read -ra selected_countries <<< "$input"
+
+# Print the selected countries
+echo "You selected the following countries:"
+for i in "${selected_countries[@]}"
+do
+  printf "%s\n" "${countries[i-1]}"
+done
+break_line
+
+# Store the selected country mirrors in an array
+selected_mirrors=()
+for i in "${selected_countries[@]}"
+do
+  case "${countries[i-1]}" in
+    "Argentina") selected_mirrors+=( "${Argentina_mirrors[@]}" ) ;;
+    "Armenia") selected_mirrors+=( "${Armenia_mirrors[@]}" ) ;;
+    "Australia") selected_mirrors+=( "${Australia_mirrors[@]}" ) ;;
+    "Austria") selected_mirrors+=( "${Austria_mirrors[@]}" ) ;;
+    "Belarus") selected_mirrors+=( "${Belarus_mirrors[@]}" ) ;;
+    "Belgium") selected_mirrors+=( "${Belgium_mirrors[@]}" ) ;;
+    "Brazil") selected_mirrors+=( "${Brazil_mirrors[@]}" ) ;;
+    "Bulgaria") selected_mirrors+=( "${Bulgaria_mirrors[@]}" ) ;;
+    "Cambodia") selected_mirrors+=( "${Cambodia_mirrors[@]}" ) ;;
+    "Canada") selected_mirrors+=( "${Canada_mirrors[@]}" ) ;;
+    "Chile") selected_mirrors+=( "${Chile_mirrors[@]}" ) ;;
+    "Costa_Rica") selected_mirrors+=( "${Costa_Rica_mirrors[@]}" ) ;;
+    "Croatia") selected_mirrors+=( "${Croatia_mirrors[@]}" ) ;;
+    "Czech_Republic") selected_mirrors+=( "${Czech_Republic_mirrors[@]}" ) ;;
+    "Denmark") selected_mirrors+=( "${Denmark_mirrors[@]}" ) ;;
+    "Estonia") selected_mirrors+=( "${Estonia_mirrors[@]}" ) ;;
+    "Finland") selected_mirrors+=( "${Finland_mirrors[@]}" ) ;;
+    "France") selected_mirrors+=( "${France_mirrors[@]}" ) ;;
+    "Georgia") selected_mirrors+=( "${Georgia_mirrors[@]}" ) ;;
+    "Germany") selected_mirrors+=( "${Germany_mirrors[@]}" ) ;;
+    "Greece") selected_mirrors+=( "${Greece_mirrors[@]}" ) ;;
+    "Hong_Kong") selected_mirrors+=( "${Hong_Kong_mirrors[@]}" ) ;;
+    "Hungary") selected_mirrors+=( "${Hungary_mirrors[@]}" ) ;;
+    "Iceland") selected_mirrors+=( "${Iceland_mirrors[@]}" ) ;;
+    "India") selected_mirrors+=( "${India_mirrors[@]}" ) ;;
+    "Indonesia") selected_mirrors+=( "${Indonesia_mirrors[@]}" ) ;;
+    "Iran") selected_mirrors+=( "${Iran_mirrors[@]}" ) ;;
+    "Israel") selected_mirrors+=( "${Israel_mirrors[@]}" ) ;;
+    "Italy") selected_mirrors+=( "${Italy_mirrors[@]}" ) ;;
+    "Japan") selected_mirrors+=( "${Japan_mirrors[@]}" ) ;;
+    "Kazakhstan") selected_mirrors+=( "${Kazakhstan_mirrors[@]}" ) ;;
+    "New_Caledonia") selected_mirrors+=( "${New_Caledonia_mirrors[@]}" ) ;;
+    "Kenya") selected_mirrors+=( "${Kenya_mirrors[@]}" ) ;;
+    "Latvia") selected_mirrors+=( "${Latvia_mirrors[@]}" ) ;;
+    "Lithuania") selected_mirrors+=( "${Lithuania_mirrors[@]}" ) ;;
+    "Luxembourg") selected_mirrors+=( "${Luxembourg_mirrors[@]}" ) ;;
+    "Macedonia") selected_mirrors+=( "${Macedonia_mirrors[@]}" ) ;;
+    "Moldova") selected_mirrors+=( "${Moldova_mirrors[@]}" ) ;;
+    "Netherlands") selected_mirrors+=( "${Netherlands_mirrors[@]}" ) ;;
+    "New_Zealand") selected_mirrors+=( "${New_Zealand_mirrors[@]}" ) ;;
+    "Norway") selected_mirrors+=( "${Norway_mirrors[@]}" ) ;;
+    "Peoples_Republic_of_China") selected_mirrors+=( "${Peoples_Republic_of_China_mirrors[@]}" ) ;;
+    "Poland") selected_mirrors+=( "${Poland_mirrors[@]}" ) ;;
+    "Portugal") selected_mirrors+=( "${Portugal_mirrors[@]}" ) ;;
+    "Republic_of_China") selected_mirrors+=( "${Republic_of_China_mirrors[@]}" ) ;;
+    "Reunion") selected_mirrors+=( "${Reunion_mirrors[@]}" ) ;;
+    "Romania") selected_mirrors+=( "${Romania_mirrors[@]}" ) ;;
+    "Russia") selected_mirrors+=( "${Russia_mirrors[@]}" ) ;;
+    "Serbia") selected_mirrors+=( "${Serbia_mirrors[@]}" ) ;;
+    "Singapore") selected_mirrors+=( "${Singapore_mirrors[@]}" ) ;;
+    "Slovakia") selected_mirrors+=( "${Slovakia_mirrors[@]}" ) ;;
+    "Slovenia") selected_mirrors+=( "${Slovenia_mirrors[@]}" ) ;;
+    "South_Africa") selected_mirrors+=( "${South_Africa_mirrors[@]}" ) ;;
+    "South_Korea") selected_mirrors+=( "${South_Korea_mirrors[@]}" ) ;;
+    "Spain") selected_mirrors+=( "${Spain_mirrors[@]}" ) ;;
+    "Sweden") selected_mirrors+=( "${Sweden_mirrors[@]}" ) ;;
+    "Switzerland") selected_mirrors+=( "${Switzerland_mirrors[@]}" ) ;;
+    "Thailand") selected_mirrors+=( "${Thailand_mirrors[@]}" ) ;;
+    "Turkey") selected_mirrors+=( "${Turkey_mirrors[@]}" ) ;;
+    "Ukraine") selected_mirrors+=( "${Ukraine_mirrors[@]}" ) ;;
+    "United_Kingdom") selected_mirrors+=( "${United_Kingdom_mirrors[@]}" ) ;;
+    "United_States") selected_mirrors+=( "${United_States_mirrors[@]}" ) ;;
+    "Vietnam") selected_mirrors+=( "${Vietnam_mirrors[@]}" ) ;;
+    *) echo "Country not found"
+  esac
+done
+
+# Function to ping a mirror and return the average response time
+ping_mirror() {
+	response_time=$(ping -c 5 "$1" | tail -1| awk '{print $4}' | cut -d '/' -f 2) || "-1"
+	echo "$response_time"
+}
+
+
+# Create an array to store the response times and mirror names
+response_times_and_mirrors=()
+
+# Ping each mirror and store the response time and mirror name in the array
+for mirror in "${selected_mirrors[@]}"
+do
+  echo "Pinging mirror: $mirror"
+  response_time=$(ping_mirror "$mirror")
+  if [ "$response_time" == "" ]
+  then
+    echo "Could not ping $mirror"
+    break_line
+  else
+    response_times_and_mirrors+=("$response_time $mirror")
+    echo "Response time: $response_time ms"
+    break_line
+  fi
+done
+
+# Sort the array based on the response time
+IFS=$'\n' sorted_response_times_and_mirrors=($(sort -n <<<"${response_times_and_mirrors[*]}"))
+unset IFS
+
+# Print the top 3 elements of the sorted array
+echo "Top 3 mirrors with the lowest response times:"
+for i in {0..2}
+do
+  echo "${sorted_response_times_and_mirrors[i]}"
+done
+break_line
+
+msg "To add a mirror to Debian, you can add the mirror's address to the system's package manager's configuration file, typically located in /etc/apt/sources.list. You can do this by using a text editor, such as nano or vim, and adding a new line with the format 'deb http://mirror.example.com/debian/ release main' at the bottom of the file, where release is the version of Debian you are using (e.g. stable, testing) and main is the component. After that you should run 'sudo apt update' to refresh the package list from the new mirror."
+
+msg "${NOFORMAT}"
